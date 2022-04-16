82c351a2fc7ebf45d663e214a1bf1942411b50a0
SAAGAR <38255285+Sagar1555@users.noreply.github.com>
2022-04-16 22:53:55 +0545

    chapter 6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+import Home from './Home';
+import Navbar from './Navbar.js';
 function App() {
+
+  const title='Welcome to new blog  ';
+  const liked = 50;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+     <div className="content">
+      <h1> App Component</h1>
+      <Home />
+
+     </div>
     </div>
   );
 }
